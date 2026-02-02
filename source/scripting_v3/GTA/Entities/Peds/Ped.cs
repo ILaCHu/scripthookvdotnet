9f29407569e99f221737b3401812c9f3da2bf16b
ILaCHu <67556774+ILaCHu@users.noreply.github.com>
2026-02-02 15:31:37 +0200

    Add Handle parameter to SetIKTarget method calls
    The function was missing the Ped pointer.

--- source/scripting_v3/GTA/Entities/Peds/Ped.cs
+++ source/scripting_v3/GTA/Entities/Peds/Ped.cs
@@ -3187,7 +3187,7 @@ namespace GTA
         public void SetIKTarget(IKPart ikPart, PedBone targetBone, Vector3 targetOffset, IKTargetFlags flags,
             int blendInTimeMS = -1, int blendOutTimeMS = -1)
         {
-            Function.Call(Hash.SET_IK_TARGET, (int)ikPart, targetBone.Owner, targetBone.Tag, targetOffset.X,
+            Function.Call(Hash.SET_IK_TARGET, Handle, (int)ikPart, targetBone.Owner, targetBone.Tag, targetOffset.X,
                 targetOffset.Y, targetOffset.Z, (int)flags, blendInTimeMS, blendOutTimeMS);
         }
 
@@ -3215,7 +3215,7 @@ namespace GTA
         public void SetIKTarget(IKPart ikPart, EntityBone targetBone, Vector3 targetOffset, IKTargetFlags flags,
             int blendInTimeMS = -1, int blendOutTimeMS = -1)
         {
-            Function.Call(Hash.SET_IK_TARGET, (int)ikPart, targetBone.Owner, targetBone.Tag, targetOffset.X,
+            Function.Call(Hash.SET_IK_TARGET, Handle, (int)ikPart, targetBone.Owner, targetBone.Tag, targetOffset.X,
                 targetOffset.Y, targetOffset.Z, (int)flags, blendInTimeMS, blendOutTimeMS);
         }
 
@@ -3254,7 +3254,7 @@ namespace GTA
         public void SetIKTarget(IKPart ikPart, Entity targetEntity, int boneTag, Vector3 targetOffset,
             IKTargetFlags flags, int blendInTimeMS = -1, int blendOutTimeMS = -1)
         {
-            Function.Call(Hash.SET_IK_TARGET, (int)ikPart, targetEntity, boneTag, targetOffset.X, targetOffset.Y,
+            Function.Call(Hash.SET_IK_TARGET, Handle, (int)ikPart, targetEntity, boneTag, targetOffset.X, targetOffset.Y,
                 targetOffset.Z, (int)flags, blendInTimeMS, blendOutTimeMS);
         }
 
@@ -3278,7 +3278,7 @@ namespace GTA
         public void SetIKTarget(IKPart ikPart, Vector3 target, IKTargetFlags flags, int blendInTimeMS = -1,
             int blendOutTimeMS = -1)
         {
-            Function.Call(Hash.SET_IK_TARGET, (int)ikPart, 0, -1, target.X, target.Y, target.Z, (int)flags,
+            Function.Call(Hash.SET_IK_TARGET, Handle, (int)ikPart, 0, -1, target.X, target.Y, target.Z, (int)flags,
                 blendInTimeMS, blendOutTimeMS);
         }
 
